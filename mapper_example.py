f16997c9ee5433e34fe644de0aca6425c428e778
Anton Nikulin <dr.tallin@gmail.com>
2013-12-30 19:56:40 +0200

    better mapper_example

--- mapper_example.py
+++ mapper_example.py
@@ -9,19 +9,17 @@ def mapper():
         data = line.strip().split("\t")
 
         # This is the place you need to do some defensive programming
-        # what if there are not exactly 6 fields in that line?
         # YOUR CODE HERE
-        if not len(data) == 6:
-            continue
+        if len(data) == 6:            
         
-        # this next line is called 'multiple assignment' in Python
-        # this is not really necessary, we could access the data
-        # with data[2] and data[5], but we do this for conveniency
-        # and to make the code easier to read
-        date, time, store, item, cost, payment = data
+            # this next line is called 'multiple assignment' in Python
+            # this is not really necessary, we could access the data
+            # with data[2] and data[5], but we do this for conveniency
+            # and to make the code easier to read
+            date, time, store, item, cost, payment = data
         
-        # Now print out the data that will be passed to the reducer
-        print "{0}\t{1}".format(store, cost)
+            # Now print out the data that will be passed to the reducer
+            print "{0}\t{1}".format(store, cost)
         
 test_text = """2013-10-09\t13:22\tMiami\tBoots\t99.95\tVisa
 2013-10-09\t13:22\tNew York\tDVD\t9.50\tMasterCard
@@ -31,9 +29,9 @@ test_text = """2013-10-09\t13:22\tMiami\tBoots\t99.95\tVisa
 
 # This function allows you to test the mapper with the provided test string
 def main():
-	import StringIO
-	sys.stdin = StringIO.StringIO(test_text)
-	mapper()
-	sys.stdin = sys.__stdin__
+    import StringIO
+    sys.stdin = StringIO.StringIO(test_text)
+    mapper()
+    sys.stdin = sys.__stdin__
 
 main()
